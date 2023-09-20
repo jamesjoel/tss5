aa8707e305cfb569a664d34b7270ec9802873ed6
jamesjoel <jamesjoelj@gmail.com>
2023-09-20 18:47:18 +0530

    ejs day 2

--- burhanuddin/app.js
+++ burhanuddin/app.js
@@ -4,15 +4,11 @@ var app = express();
 app.use(express.static(__dirname+"/assets"))
 
 
-app.listen(3000, ()=>{
-    console.log("server running");
-});
+
 
 app.get("/about", (x , y)=>{
     y.sendFile(__dirname+"/about.html");
 })
-
-
 app.get("/home", (x , y)=>{
     y.sendFile(__dirname+"/home.html");
 })
@@ -24,3 +20,7 @@ app.get("/", (x , y)=>{
 app.get("/contact", (x , y)=>{
     y.sendFile(__dirname+"/contact.html");
 })
+
+app.listen(3000, ()=>{
+    console.log("server running");
+});
