23ad4d3b99c7943311e4f92a7b49263559cd39e1
jamesjoel <jamesjoelj@gmail.com>
2023-09-22 18:15:43 +0530

    form in express

--- Sadiq/slicing/app.js
+++ Sadiq/slicing/app.js
@@ -10,53 +10,55 @@ app.get("/", (req, res)=>{
 
 app.get("/about", (req, res)=>{
 
-    // let table = [
-    //     {
-    //         name : "rahul",
-    //         age : 46,
-    //         city : "indore"
-    //     },
-    //     {
-    //         name : "arjun",
-    //         age : 36,
-    //         city : "mumbai"
-    //     },
-    //     {
-    //         name : "sourav",
-    //         age : 54,
-    //         city : "pune"
-    //     },
-    //     {
-    //         name : "piyush",
-    //         age : 75,
-    //         city : "surat"
-    //     }
-    // ]
+    let table = [
+        {
+            name : "rahul",
+            age : 46,
+            city : "indore"
+        },
+        {
+            name : "arjun",
+            age : 36,
+            city : "mumbai"
+        },
+        {
+            name : "sourav",
+            age : 54,
+            city : "pune",
+        },
+        {
+            name : "piyush",
+            age : 75,
+            city : "surat"
+        }
+    ]
 
-    // let course = [
-    //     {
-    //         subject : "Mathametics",
-    //         marks : 80
-    //     },
-    //     {
-    //         subject : "Chemistry",
-    //         marks : 78
-    //     },
-    //     {
-    //         subject : "Biology",
-    //         marks : 59
-    //     },
-    //     {
-    //         subject : "Physics",
-    //         marks : 70
-    //     },
-    // ]
+    let course = [
+        {
+            subject : "Mathametics",
+            marks : 80
+        },
+        {
+            subject : "Chemistry",
+            marks : 78
+        },
+        {
+            subject : "Biology",
+            marks : 59
+        },
+        {
+            subject : "Physics",
+            marks : 70
+        },
+    ]
 
-    // let pagedata = {data1 : table, data2 : course}
-    res.render("pages/about")
+    let pagedata = {data1 : table, data2 : course}
+    res.render("pages/about", pagedata)
 })
 
 const port = 3000;
 app.listen(port, ()=>{
-    console.log(`server is running with ${port} port`)
+    // console.log(`server is running with ${port} port`)
+    
+    console.log("server running ", port);
 })
