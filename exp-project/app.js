d64fc080885efad4cb8699330bd846195909a8eb
jamesjoel <jamesjoelj@gmail.com>
2023-09-25 18:45:14 +0530

    mongoose

--- exp-project/app.js
+++ exp-project/app.js
@@ -1,5 +1,30 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://0.0.0.0:27017/tss5_new");
+
+const mySchema = mongoose.Schema({
+    contact : String,
+    fullname : String,
+    email : String,
+    fee : Number,
+    gender : String,
+    address : String,
+    city : String,
+    hobby : []
+});
+
+const myModel = mongoose.model("student", mySchema);
+
+const demoSchema = mongoose.Schema({
+    name : String,
+    salary : Number,
+    city : String
+})
+
+const demoModal = mongoose.model("demo", demoSchema);
+
 
 app.use(express.static(__dirname+"/assets"));
 app.set("view engine", "ejs");
@@ -7,8 +32,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
 
+app.post("/demo", (req, res)=>{
+    demoModal.create(req.body);
+    res.redirect("/");
+})
+
+
 app.post("/save", (req, res)=>{
-    console.log(req.body);
+    myModel.create(req.body);
+    res.redirect("/");
 })
 
 app.get("/", (req, res)=>{
