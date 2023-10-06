c3617dfb9d955fb2299168f3c997c44ce4019177
Burhanuddin785 <burhandewas989@gmail.com>
2023-10-06 20:02:27 +0530

    new project by the name of proj-6,10

---
+++ burhanuddin/proj-6,10/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express();
+
+app.use(express.static(__dirname+"/assets"));
+app.set("view engine","ejs");
+
+app.get("/", (req, res)=>{
+    res.render("home")
+});
+
+
+
+const port=3000;
+
+app.listen(port, ()=>{
+    console.log("server running with port"+port)
+});
