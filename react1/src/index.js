edb5209d5bb1436b4049b17a967bf9390306eaef
jamesjoel <jamesjoelj@gmail.com>
2023-10-16 18:44:38 +0530

    react day 1

---
+++ react1/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('box'));
+root.render(
+  <>
+    <h1>Indore</h1>
+    <h2>The Stepping Stone</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit natus nesciunt deleniti. Perspiciatis, architecto. Aliquam blanditiis quaerat odio, tempora commodi fugit aspernatur provident nesciunt quis, rerum veritatis eum voluptate sapiente!</p>
+  </>
+);
