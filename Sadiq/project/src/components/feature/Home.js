c9c8ed0e9eca79ecbb43956c16dd949723686bfb
Sadiq53 <jafarussadiq098@gmail.com>
2023-10-28 20:09:25 +0530

    workin on project

---
+++ Sadiq/project/src/components/feature/Home.js
@@ -0,0 +1,342 @@
+/* eslint-disable */
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <>
+        <section className="banner spad">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-7 offset-lg-4">
+                    <div className="banner__item">
+                        <div className="banner__item__pic">
+                            <img src="img/banner/banner-1.jpg" alt="" />
+                        </div>
+                        <div className="banner__item__text">
+                            <h2>Clothing Collections 2030</h2>
+                            <a href="#">Shop now</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-5">
+                    <div className="banner__item banner__item--middle">
+                        <div className="banner__item__pic">
+                            <img src="img/banner/banner-2.jpg" alt="" />
+                        </div>
+                        <div className="banner__item__text">
+                            <h2>Accessories</h2>
+                            <a href="#">Shop now</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-7">
+                    <div className="banner__item banner__item--last">
+                        <div className="banner__item__pic">
+                            <img src="img/banner/banner-3.jpg" alt="" />
+                        </div>
+                        <div className="banner__item__text">
+                            <h2>Shoes Spring 2030</h2>
+                            <a href="#">Shop now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section className="product spad">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <ul className="filter__controls">
+                        <li className="active" data-filter="*">Best Sellers</li>
+                        <li data-filter=".new-arrivals">New Arrivals</li>
+                        <li data-filter=".hot-sales">Hot Sales</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row product__filter">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div className="product__item">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-1.jpg">
+                            <span className="label">New</span>
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Piqué Biker Jacket</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$67.24</h5>
+                            <div className="product__color__select">
+                                <label for="pc-1">
+                                    <input type="radio" id="pc-1" />
+                                </label>
+                                <label className="active black" for="pc-2">
+                                    <input type="radio" id="pc-2" />
+                                </label>
+                                <label className="grey" for="pc-3">
+                                    <input type="radio" id="pc-3" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div className="product__item">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Piqué Biker Jacket</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$67.24</h5>
+                            <div className="product__color__select">
+                                <label for="pc-4">
+                                    <input type="radio" id="pc-4" />
+                                </label>
+                                <label className="active black" for="pc-5">
+                                    <input type="radio" id="pc-5" />
+                                </label>
+                                <label className="grey" for="pc-6">
+                                    <input type="radio" id="pc-6" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div className="product__item sale">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
+                            <span className="label">Sale</span>
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Multi-pocket Chest Bag</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$43.48</h5>
+                            <div className="product__color__select">
+                                <label for="pc-7">
+                                    <input type="radio" id="pc-7" />
+                                </label>
+                                <label className="active black" for="pc-8">
+                                    <input type="radio" id="pc-8" />
+                                </label>
+                                <label className="grey" for="pc-9">
+                                    <input type="radio" id="pc-9" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div className="product__item">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Diagonal Textured Cap</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$60.9</h5>
+                            <div className="product__color__select">
+                                <label for="pc-10">
+                                    <input type="radio" id="pc-10" />
+                                </label>
+                                <label className="active black" for="pc-11">
+                                    <input type="radio" id="pc-11" />
+                                </label>
+                                <label className="grey" for="pc-12">
+                                    <input type="radio" id="pc-12" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div className="product__item">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-5.jpg">
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Lether Backpack</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$31.37</h5>
+                            <div className="product__color__select">
+                                <label for="pc-13">
+                                    <input type="radio" id="pc-13" />
+                                </label>
+                                <label className="active black" for="pc-14">
+                                    <input type="radio" id="pc-14" />
+                                </label>
+                                <label className="grey" for="pc-15">
+                                    <input type="radio" id="pc-15" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div className="product__item sale">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-6.jpg">
+                            <span className="label">Sale</span>
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Ankle Boots</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$98.49</h5>
+                            <div className="product__color__select">
+                                <label for="pc-16">
+                                    <input type="radio" id="pc-16" />
+                                </label>
+                                <label className="active black" for="pc-17">
+                                    <input type="radio" id="pc-17" />
+                                </label>
+                                <label className="grey" for="pc-18">
+                                    <input type="radio" id="pc-18" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div className="product__item">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-7.jpg">
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>T-shirt Contrast Pocket</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$49.66</h5>
+                            <div className="product__color__select">
+                                <label for="pc-19">
+                                    <input type="radio" id="pc-19" />
+                                </label>
+                                <label className="active black" for="pc-20">
+                                    <input type="radio" id="pc-20" />
+                                </label>
+                                <label className="grey" for="pc-21">
+                                    <input type="radio" id="pc-21" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div className="product__item">
+                        <div className="product__item__pic set-bg" data-setbg="img/product/product-8.jpg">
+                            <ul className="product__hover">
+                                <li><a href="#"><img src="img/icon/heart.png" alt="" /></a></li>
+                                <li><a href="#"><img src="img/icon/compare.png" alt="" /> <span>Compare</span></a></li>
+                                <li><a href="#"><img src="img/icon/search.png" alt="" /></a></li>
+                            </ul>
+                        </div>
+                        <div className="product__item__text">
+                            <h6>Basic Flowing Scarf</h6>
+                            <a href="#" className="add-cart">+ Add To Cart</a>
+                            <div className="rating">
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                                <i className="fa fa-star-o"></i>
+                            </div>
+                            <h5>$26.28</h5>
+                            <div className="product__color__select">
+                                <label for="pc-22">
+                                    <input type="radio" id="pc-22" />
+                                </label>
+                                <label className="active black" for="pc-23">
+                                    <input type="radio" id="pc-23" />
+                                </label>
+                                <label className="grey" for="pc-24">
+                                    <input type="radio" id="pc-24" />
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Home
--- Sadiq/react1/src/components/feature/Calculator.js
+++ Sadiq/react1/src/components/feature/Calculator.js
@@ -1,59 +1,25 @@
 import { useState } from "react";
 let Calculator = ()=>{
 
-    let [Num, setNum1] = useState(0)
-    let Number1 = ()=>{
-        setNum1(Num*10+1)
-    }
-    let Number2 = ()=>{
-        setNum1(Num*10+2)
-    }
-    let Number3 = ()=>{
-        setNum1(Num*10+3)
-    }
-    let Number4 = ()=>{
-        setNum1(Num*10+4)
-    }
-    let Number5 = ()=>{
-        setNum1(Num*10+5)
-    }
-    let Number6 = ()=>{
-        setNum1(Num*10+6)
-    }
-    let Number7 = ()=>{
-        setNum1(Num*10+7)
-    }
-    let Number8 = ()=>{
-        setNum1(Num*10+8)
-    }
-    let Number9 = ()=>{
-        setNum1(Num*10+9)
-    }
-    let Number0 = ()=>{
-        setNum1(Num*10+0)
-    }
-    let Clear = ()=>{
-        setNum1(0)
-    }
+    let [Num, setNum] = useState("")
 
-    let Result = (operation)=>{
-        operation += Num
-        setNum1(operation)
+    let handleButtonClick = (btnText)=>{
+        if (btnText === 'x') {
+            btnText = '*';
+        }
+
+        if (btnText === '÷') {
+            btnText = '/';
+        }
+
+        setNum(Num + btnText);
     }
 
     let backSpace = ()=>{
-        let newNum = Num.toString().split("");
+        let newNum = Num.split("");
         newNum.splice(newNum.length-1,1);
-        newNum = parseInt(newNum.join(""));
-        setNum1(newNum)
-    }
-
-    let addition =()=>{
-        let addCase1 = Num.toString().split("");
-        let addCase2 = [...addCase1]
-        let addCase3 = parseInt(addCase2.join(""))
-        setNum1(0)
-        console.log(addCase3)
+        newNum = newNum.join("");
+        setNum(newNum)
     }
 
 
@@ -65,37 +31,37 @@ let Calculator = ()=>{
                         <div className="card">
                             <div className="card-header bg-success text-center"><h2 className="text-light">CALCULATOR</h2></div>
                             <div className="card-body bg-dark">
-                                <input type="number" value={Num} className=" text-center form-control" />
+                                <input type="number" value={Num} placeholder="0" className=" text-center form-control" />
                                 <br />
                                 <div className="container p-0">
                                     <div className="row">
                                         <div className="col-md-5 text-center">
                                             <div className="btn-group">
-                                                <button onClick={Number1} className="btn btn-success text-light" style={{fontSize : "22px"}}>1</button>
-                                                <button onClick={Number2} className="btn text-light btn-success" style={{fontSize : "22px"}}>2</button>
-                                                <button onClick={Number3} className="btn btn-success text-light" style={{fontSize : "22px"}}>3</button>
-                                                <button onClick={Number4} className="btn btn-success text-light" style={{fontSize : "22px"}}>4</button>
-                                                <button onClick={Number5} className="btn btn-success text-light" style={{fontSize : "22px"}}>5</button>
-                                                <button onClick={Number6} className="btn btn-success text-light" style={{fontSize : "22px"}}>6</button>
-                                                <button onClick={Number7} className="btn btn-success text-light" style={{fontSize : "22px"}}>7</button>
-                                                <button onClick={Number8} className="btn btn-success text-light" style={{fontSize : "22px"}}>8</button>
-                                                <button onClick={Number9} className="btn btn-success text-light" style={{fontSize : "22px"}}>9</button>
-                                                <button onClick={Number0} className="btn btn-success text-light" style={{fontSize : "22px"}}>0</button>
+                                                <button onClick={()=>{handleButtonClick("1")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>1</button>
+                                                <button onClick={()=>{handleButtonClick("2")}} className="btn text-light btn-success" style={{fontSize : "22px"}}>2</button>
+                                                <button onClick={()=>{handleButtonClick("3")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>3</button>
+                                                <button onClick={()=>{handleButtonClick("4")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>4</button>
+                                                <button onClick={()=>{handleButtonClick("5")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>5</button>
+                                                <button onClick={()=>{handleButtonClick("6")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>6</button>
+                                                <button onClick={()=>{handleButtonClick("7")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>7</button>
+                                                <button onClick={()=>{handleButtonClick("8")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>8</button>
+                                                <button onClick={()=>{handleButtonClick("9")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>9</button>
+                                                <button onClick={()=>{handleButtonClick("0")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>0</button>
                                             </div>
                                         </div>
                                         <div className="col-md-3 text-center">
                                             <div className="btn-group">
-                                                <button className="btn btn-success text-light" style={{fontSize : "22px"}}>x</button>
-                                                <button className="btn btn-success text-light" style={{fontSize : "22px"}}>/</button>
-                                                <button onClick={addition} className="btn btn-success text-light" style={{fontSize : "22px"}}>+</button>
-                                                <button className="btn btn-success text-light" style={{fontSize : "22px"}}>-</button>
+                                                <button onClick={()=>{handleButtonClick("x")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>x</button>
+                                                <button onClick={()=>{handleButtonClick("/")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>/</button>
+                                                <button onClick={()=>{handleButtonClick("+")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>+</button>
+                                                <button onClick={()=>{handleButtonClick("-")}} className="btn btn-success text-light" style={{fontSize : "22px"}}>-</button>
                                             </div>
                                         </div>
                                         <div className="col-md-2 text-center">
                                             <div className="btn-group">
-                                                <button onClick={Result} className="btn btn-outline-success text-light" style={{fontSize : "20px"}}>=</button>
+                                                <button id="eval" onClick={()=>{setNum(eval(Num))}} className="btn btn-outline-success text-light" style={{fontSize : "20px"}}>=</button>
                                                 <button onClick={backSpace} className="btn btn-outline-success text-light" style={{fontSize : "20px"}}>BackSpace</button>
-                                                <button onClick={Clear} className="btn btn-outline-danger text-light" style={{fontSize : "20px"}}>Clear</button>
+                                                <button onClick={()=>{setNum("")}} className="btn btn-outline-danger text-light" style={{fontSize : "20px"}}>Clear</button>
                                             </div>
                                         </div>
                                     </div>
