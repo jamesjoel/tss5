8478ff907b23a9e136c011f478259fe000705b44
jamesjoel <jamesjoelj@gmail.com>
2023-10-30 18:52:01 +0530

    props in react

--- project/src/components/feature/About.js
+++ project/src/components/feature/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Subscribe from '../shared/Subscribe'
+import Box from '../shared/Box'
 const About = () => {
+ 
   return (
     <>
-    
     <div className="container" style={{minHeight : "700px", marginTop : "150px"}}>
+ 
         <div className="row">
             <div className="col-md-12">
                 <h2>About Page</h2>
--- project/src/routes/AllRoutes.js
+++ project/src/routes/AllRoutes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../components/feature/Home'
 import About from '../components/feature/About'
+import Login from '../components/feature/Login'
 const AllRoutes = () => {
   return (
     <>
         <Routes>
             <Route path='' element={<Home />} />
             <Route path='about' element={<About />} />
+            <Route path='login' element={<Login />} />
         </Routes>
     </>
   )
