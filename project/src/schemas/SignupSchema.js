3b43069f8f5f49e9360bbef27a2f20897c868019
jamesjoel <jamesjoelj@gmail.com>
2023-11-10 18:13:38 +0530

    yup validation

---
+++ project/src/schemas/SignupSchema.js
@@ -0,0 +1,15 @@
+import * as YUP from 'yup';
+
+let SingupSchema = YUP.object({
+    name : YUP.string().required("Insert Your Full Name"),
+    email : YUP.string().email("This Email Id is Incorrect").required("Insert Your Email/Username"),
+    password : YUP.string().required("Insert Your Password"),
+    repassword : YUP.string().oneOf([YUP.ref("password")], "Password and Re-Password should be same").required("Insert Your Re-Password"),
+    gender : YUP.string().required("Select Your Gender"),
+    address : YUP.string().required("Insert Your Address"),
+    state : YUP.string().required("Select Your State"),
+    city : YUP.string().required("Select Your City"),
+    contact : YUP.string().required("Insert Your Contact Number")
+})
+
+export default SingupSchema;
