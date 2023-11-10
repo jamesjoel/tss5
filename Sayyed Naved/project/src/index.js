48f4d531153780437c0836e7beb260b3bed56272
Naved06 <navedsayyed14@gmail.com>
2023-11-10 11:14:03 +0530

    naved

--- Sayyed Naved/project/src/index.js
+++ Sayyed Naved/project/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';const root = ReactDOM.createRoot(document.getElementById('root'));
+import {BrowserRouter} from  'react-router-dom'
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
