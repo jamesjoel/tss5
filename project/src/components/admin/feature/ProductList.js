410a26014ff0cf52d233cf58fae894afafb9dc65
jamesjoel <jamesjoelj@gmail.com>
2024-01-03 18:22:52 +0530

    delete complete

--- project/src/components/admin/feature/ProductList.js
+++ project/src/components/admin/feature/ProductList.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import axios from 'axios'
 import { API_URL} from '../../../util/API'
 
@@ -6,16 +6,31 @@ import { API_URL} from '../../../util/API'
 
 const ProductList = () => {
 
-  let [pro, setPro] = useState([]);
+  let [pro, setPro] = useState({});
+  let btn = useRef();
+
+  let [allPro, setAllPro] = useState([]);
   useEffect(()=>{
 
     axios.get(`${API_URL}/product`).then(response=>{
-      setPro(response.data.result);
+      setAllPro(response.data.result);
     })
 
   },[])
 
+  let askDelete = (obj)=>{
+    setPro(obj);
+  }
+  let confDelete = async()=>{
+    await axios.delete(`${API_URL}/product/${pro._id}`);
+    setAllPro(()=>{
+      return allPro.filter(value => value._id != pro._id);
+    })
+    btn.current.click();
+  }
+
   return (
+    <>
     <div className="container my-3">
       <div className="row">
         <div className="col-md-12">
@@ -28,11 +43,12 @@ const ProductList = () => {
                 <th>Price</th>
                 <th>Category</th>
                 <th>Sub-Category</th>
+                <th>Delete</th>
               </tr>
             </thead>
             <tbody>
               {
-                pro.map((value, index)=>{
+                allPro.map((value, index)=>{
                   return(
                     <tr key={value._id}>
                       <td>{index+1}</td>
@@ -40,6 +56,7 @@ const ProductList = () => {
                       <td>{value.price}</td>
                       <td>{value.category}</td>
                       <td>{value.subcategory}</td>
+                      <td><button data-target="#delModal" data-toggle="modal" onClick={()=>askDelete(value)} className='btn btn-danger btn-sm'>Delete</button></td>
                     </tr>
                   )
                 })
@@ -49,6 +66,26 @@ const ProductList = () => {
         </div>
       </div>
     </div>
+
+    <div className="modal fade" id="delModal">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h4>Delete Product</h4>
+          </div>
+          <div className="modal-body">
+            <p>Are you sure want to delete <strong style={{fontWeight : "bold"}}>{ pro ? pro.title : '' }</strong></p>
+          </div>
+          <div className="modal-footer">
+            <button className='btn btn-danger btn-sm' onClick={confDelete}>Delete</button>
+            <button ref={btn} data-dismiss="modal" className='btn btn-dark btn-sm'>Close</button>
+          </div>
+        </div>
+      </div>
+    </div>           
+
+    </>
+
   )
 }
 
