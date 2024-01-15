50260f99581b3e05e9ee608b29df8ba4dff49003
RifakatAnsari <rifakata474@gmail.com>
2024-01-15 17:24:30 +0530

    crud

--- rifakat ansari/project-api/routes/allRoutes.js
+++ rifakat ansari/project-api/routes/allRoutes.js
@@ -3,4 +3,7 @@ const routes = require ("express").Router();
 routes.use("/api/student", require("../controllers/StudentController"));
 routes.use("/api/city",require("../controllers/CityController"));
 routes.use("/api/User",require("../controllers/UserController"));
+routes.use("/api/auth",require("../controllers/AuthController"));
+routes.use("/api/profile",require("../controllers/ProfileController"));
+
 module.exports = routes;
